2a581eb453bf84ca68910d811d7bd3f7165ec28a
azio7 <azio7@outlook.com>
2023-11-06 16:58:55 +0800

    fix panic on android

--- main.go
+++ main.go
@@ -2,27 +2,21 @@ package main
 
 import (
 	"bytes"
+	"flag"
 	"fmt"
 	"log"
-	"os"
 
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
 	"github.com/google/gopacket/pcap"
 )
 
-// var (
-// 	routine int
-// 	count   int
-// )
-
 func analysePacket(packet gopacket.Packet) {
 	if ip4Layer := packet.Layer(layers.LayerTypeIPv4); ip4Layer != nil {
 		ip4 := ip4Layer.(*layers.IPv4)
 
 		// tcp
-		if ip4.NextLayerType() == layers.LayerTypeTCP {
-			tcpLayer := packet.Layer(layers.LayerTypeTCP)
+		if tcpLayer := packet.Layer(layers.LayerTypeTCP); tcpLayer != nil {
 			tcp := tcpLayer.(*layers.TCP)
 
 			// is HTTP request or response
@@ -34,13 +28,12 @@ func analysePacket(packet gopacket.Packet) {
 				}
 			}
 			// udp
-		} else if ip4.NextLayerType() == layers.LayerTypeUDP {
-			udpLayer := packet.Layer(layers.LayerTypeUDP)
+		} else if udpLayer := packet.Layer(layers.LayerTypeUDP); udpLayer != nil {
 			udp := udpLayer.(*layers.UDP)
 			// dns
-			if udp.NextLayerType() == layers.LayerTypeDNS {
+			if dnsLayer := packet.Layer(layers.LayerTypeDNS); dnsLayer != nil {
 				fmt.Printf("DNS     From %s:%d to %s:%d\n", ip4.SrcIP, udp.SrcPort, ip4.DstIP, udp.DstPort)
-				dnsLayer := packet.Layer(layers.LayerTypeDNS)
+
 				dns := dnsLayer.(*layers.DNS)
 				// is dns request or response
 				if dns.QR {
@@ -56,19 +49,18 @@ func analysePacket(packet gopacket.Packet) {
 			}
 		}
 	}
-	// routine += runtime.NumGoroutine()
-	// count++
 }
 
 func main() {
-	// Check if file argument is provided
-	if len(os.Args) < 2 {
-		fmt.Println("Please provide a wifi interface")
-		os.Exit(1)
-	}
+
+	var (
+		device string = "wlan0"
+	)
+	flag.StringVar(&device, "i", device, "Interface to use")
+	flag.Parse()
 
 	// Open a pcap handle for the provided interface
-	handle, err := pcap.OpenLive(os.Args[1], 16384, true, pcap.BlockForever)
+	handle, err := pcap.OpenLive(device, 16384, true, pcap.BlockForever)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -78,16 +70,7 @@ func main() {
 	packetSource.NoCopy = true
 	packetSource.Lazy = true
 
-	// count = 0
-	// routine = 0
 	for packet := range packetSource.Packets() {
-		// if count > 10000 {
-		// 	time.Sleep(500 * time.Millisecond)
-		// 	fmt.Printf("count: %d, routine: %d\n", count, routine)
-
-		// 	fmt.Printf("avg: %f\n", float32(routine)/float32(count))
-		// 	break
-		// }
 		go analysePacket(packet)
 	}
 }
